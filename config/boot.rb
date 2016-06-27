3c5362c818bd89f13fdd23a8d152fb8ade370b04
kev <kelvinchege@gmail.com>
2016-06-27 21:50:23 +0300

    changes after rake

--- config/boot.rb
+++ config/boot.rb
@@ -6,7 +6,7 @@ require 'rails/commands/server'
 module Rails
   class Server
     def default_options
-      super.merge(Host:  '192.168.24.24', Port: 3000)
+      super.merge(Host:  '192.168.0.101', Port: 3000)
     end
   end
 end
